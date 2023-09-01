534b519beb8804af99c47d995237ed5877fb248b
shubhamghimire99 <shubham0011ghimire@gmail.com>
2023-09-01 14:23:13 +0545

    chating bug fixed

--- src/main/java/com/chat/config/WebSocketConfig.java
+++ src/main/java/com/chat/config/WebSocketConfig.java
@@ -1,19 +1,34 @@
 package com.chat.config;
 
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import com.chat.controller.ChatWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketConfigurer,WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new ChatWebSocketHandler(), "/cheat");
+    }
+
+     @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
--- src/main/resources/static/JS/index.js
+++ src/main/resources/static/JS/index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	});
 });
 
-/*for chating *//*
+/*for chating */
 
 
 
